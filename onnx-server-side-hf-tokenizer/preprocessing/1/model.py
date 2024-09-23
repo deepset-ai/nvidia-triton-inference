9c3e4b3d1f88aabcfdeb1bbaa4dace56c6f64173
tstadel <60758086+tstadel@users.noreply.github.com>
2024-09-23 18:40:38 +0200

    feat: optimize template (#2)

--- onnx-server-side-hf-tokenizer/preprocessing/1/model.py
+++ onnx-server-side-hf-tokenizer/preprocessing/1/model.py
@@ -1,42 +1,56 @@
+import json
 import os
 from typing import Dict
-import numpy as np
 import triton_python_backend_utils as pb_utils
 from transformers import AutoTokenizer
+from tritonclient.utils import triton_to_np_dtype
 
 
 class TritonPythonModel:
     def initialize(self, args: Dict[str, str]) -> None:
+        self.logger = pb_utils.Logger
+        self.logger.log_info(f"Initializing tokenizer with args: {args}")
         path: str = os.path.join(args["model_repository"], args["model_version"])
         self.tokenizer = AutoTokenizer.from_pretrained(path)
-        self.logger = pb_utils.Logger
+
+        # inspect config.pbtxt
+        config = json.loads(args["model_config"])
+
+        # Output type mapping
+        self.output_types = {
+            output["name"]: triton_to_np_dtype(output["data_type"].removeprefix("TYPE_"))
+            for output in config["output"]
+        }
 
     def execute(self, requests):
         responses = []
         for request in requests:
+
+            if request.is_cancelled():
+                responses.append(
+                    pb_utils.InferenceResponse(
+                        error=pb_utils.TritonError(
+                            "Request cancelled", pb_utils.TritonError.CANCELLED
+                        )
+                    )
+                )
+                continue
+
             input_tensor = pb_utils.get_input_tensor_by_name(request, "text")
             self.logger.log_info(f"input text shape: {input_tensor.shape()}")
             
-            texts = [text.decode("utf-8") for text in input_tensor.as_numpy().tolist()]
+            texts = [text.decode("utf-8") for text in input_tensor.as_numpy()]
 
-            tokenizer_output = self.tokenizer(texts, padding=True)
-            input_ids = np.array(tokenizer_output["input_ids"])
-            attention_mask = np.array(tokenizer_output["attention_mask"])
+            tokenizer_output = self.tokenizer(texts, padding=True, truncation=True, return_tensors="np")
 
-            self.logger.log_info(f"output input_ids shape: {input_ids.shape}")
-            self.logger.log_info(f"output attention_mask shape: {attention_mask.shape}")
+            output_tensors = []
+            for output_name in self.tokenizer.model_input_names:
+                output_type = self.output_types[output_name]
+                output_value = tokenizer_output[output_name].astype(output_type)
+                self.logger.log_info(f"output {output_name} shape: {output_value.shape}")
+                output_tensor = pb_utils.Tensor(output_name, output_value)
+                output_tensors.append(output_tensor)
 
-            inference_response = pb_utils.InferenceResponse(
-                output_tensors=[
-                    pb_utils.Tensor(
-                        "input_ids",
-                        input_ids,
-                    ),
-                    pb_utils.Tensor(
-                        "attention_mask",
-                        attention_mask,
-                    ),
-                ]
-            )
+            inference_response = pb_utils.InferenceResponse(output_tensors=output_tensors)
             responses.append(inference_response)
         return responses
